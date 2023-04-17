9c3488caf56118813e2814f91e082ba1ae01ea0d
Tanvir Rahman <opensoucebd99@gmail.com>
2023-04-17 23:11:48 +0600

    multiplicationTable Done

---
+++ assignments_11/script.js
@@ -0,0 +1,10 @@
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    let product = num * i;
+    console.log(num + " x " + i + " = " + product);
+  }
+}
+
+multiplicationTable(2);
+console.log('----------------------');
+multiplicationTable(4);
